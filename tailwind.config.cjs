68f850de83bf22ce12eb602d85fcdf269cdb8cfe
RamunnoAJ <ramunnoagustin@gmail.com>
2022-11-09 13:18:47 -0300

    add: tailwind configuration for colors and fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: {
+          400: '#f1eecf',
+          500: '#eaf1bb'
+        },
+        accent: {
+          400: '#ef8405'
+        }
+      }
+    },
+    fontFamily: {
+      Montserrat: 'Montserrat Alternates'
+    }
   },
   plugins: []
 }
