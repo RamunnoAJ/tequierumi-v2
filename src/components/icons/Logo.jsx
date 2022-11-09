cde5f7243abf19544e154ead920c6c3925644bd7
RamunnoAJ <ramunnoagustin@gmail.com>
2022-11-09 13:19:22 -0300

    add: Logo icon

---
+++ src/components/icons/Logo.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import logo from '../../assets/logo.png'
+
+function Logo() {
+  return <img src={logo} alt='Logo de la empresa' className=' w-20' />
+}
+
+export default Logo
