51973163dcaf67d42c2ce7f72f0cba88a2a50457
RamunnoAJ <ramunnoagustin@gmail.com>
2022-11-10 15:19:36 -0300

    add: button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Button(props) {
+  return (
+    <button className='uppercase bg-transparent min-w-[130px] h-10 cursor-pointer transition-all duration-300 ease-in-out relative border border-black after:w-full after:h-full after:absolute after:-z-10 after:top-1 after:left-1 after:transition-all after:duration-300 after:ease-in-out after:bg-accent-400 hover:after:top-0 hover:after:left-0'>
+      {props.children}
+    </button>
+  )
+}
+
+export default Button
