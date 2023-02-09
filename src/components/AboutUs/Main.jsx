73edf9364516b981cf27f03a51e07d9cf4f60511
RamunnoAJ <ramunnoagustin@gmail.com>
2023-02-09 13:56:42 -0300

    add: about-us section

---
+++ src/components/AboutUs/Main.jsx
@@ -0,0 +1,37 @@
+export const Main = () => {
+  return (
+    <main className='w-11/12 mx-auto my-10 md:my-16 max-w-7xl'>
+      <div className='flex flex-col items-center gap-4 md:gap-8'>
+        <h1 className='text-4xl font-bold uppercase text-accent-400 md:text-6xl'>
+          Quienes Somos
+        </h1>
+        <div className='flex flex-col max-w-5xl gap-2'>
+          <p className='text-sm tracking-wide text-gray-600 md:text-lg'>
+            De pequeña me sentaba junto a mi abuela Ramona mientras ella tejía.
+            Puntada tras puntada compartíamos risas y lindos momentos. Un día se
+            despertó en mí la curiosidad por esta técnica, y ella con toda su
+            gracia y paciencia, me inició en el mágico mundo del tejido.{' '}
+          </p>
+          <p className='text-sm tracking-wide text-gray-600 md:text-lg'>
+            Desde entonces mi relación con las agujas se mantiene intacta.
+            Sucede que al tejer mi imaginación vuela, me transporta... Las
+            agujas e hilados me permiten plasmar una idea, realizar un proyecto
+            y celebrar el resultado. Tejer es plantearme desafíos y trabajar
+            para concretarlos; a veces, algunos de ellos llegan cargados de
+            dificultades y frustraciones, pero mi gran secreto es perseverar,
+            modificar lo que sea necesario para concluirlos y sentirme feliz con
+            el resultado.
+          </p>
+          <p className='text-sm tracking-wide text-gray-600 md:text-lg'>
+            En Tequierumi me acompañan mis dos hijas. Ellas son las que me
+            animaron a mostrar y divulgar en redes lo que con mucho amor hago.
+            Así se conforma Tequierumi. Desde aquí nosotras... y del otro lado
+            ustedes, con sus pedidos y deseos. Gracias por dejarnos ser parte de
+            sus mágicos momentos, regalando nuestros productos a los seres que
+            más aman.
+          </p>
+        </div>
+      </div>
+    </main>
+  )
+}
